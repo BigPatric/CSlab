20fb19f5fbe5efcbd2eb53eafcc0818bf0c35530
PatrickWu <dinosourgg@gmail.com>
2025-03-18 00:02:17 -0700

    dp_update

--- Lab1/lab1_template/asm_template/dp/dp.s
+++ Lab1/lab1_template/asm_template/dp/dp.s
@@ -13,16 +13,16 @@ loop2:
     li t2, 6
     bge t1, t2, ADI   # if j >= 6，to next i loop
 
-    # 計算 arr[2*j] 和 arr[2*j+1]
+    # read arr[2*j] & arr[2*j+1]
     slli t2, t1, 3    # t2 = j * 8 (2*j * 4)
     add t3, a0, t2    # t3 = arr + (2*j)*4
     lw t4, 0(t3)      # t4 = arr[2*j]
     lw t5, 4(t3)      # t5 = arr[2*j+1]
 
     sub t6, t0, t4    # t6 = i - arr[2*j]
-    bltz t6, ADJ      # 如果 i - arr[2*j] < 0，跳過
+    bltz t6, ADJ      # if i - arr[2*j] < 0，continue
 
-    # 讀取 dp_array[i] 和 dp_array[i - arr[2*j]]
+    # read dp_array[i] & dp_array[i - arr[2*j]]
     slli t2, t0, 2    # t2 = i * 4
     add t2, a2, t2    # t2 = dp_array + i*4
     lw t3, 0(t2)      # t3 = dp_array[i]
@@ -32,7 +32,7 @@ loop2:
     lw t4, 0(t6)      # t4 = dp_array[i - arr[2*j]]
 
     add t4, t4, t5    # t4 = dp_array[i - arr[2*j]] + arr[2*j+1]
-    bge t3, t4, ADJ   # 如果 dp_array[i] >= t4，跳過更新
+    bge t3, t4, ADJ   # if dp_array[i] >= t4，no update 
 
     sw t4, 0(t2)      # dp_array[i] = t4
 
