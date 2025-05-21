1c1b811be9c704a5a50c95f9e3ed373485d2eb38
patrickwu <dinosourgg@gmail.com>
2025-05-22 00:51:47 +0800

    fix

--- Lab4/lab4_template/Mux2to1.v
+++ Lab4/lab4_template/Mux2to1.v
@@ -2,7 +2,7 @@ module Mux2to1 #(
     parameter size = 32
 )
 (
-    input sel,
+    input [1:0]sel,
     input signed [size-1:0] s0,
     input signed [size-1:0] s1,
     output signed [size-1:0] out
