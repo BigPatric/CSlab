ae085fbb2370c331ad4173e79140bade8d080061
patrickwu <dinosourgg@gmail.com>
2025-05-23 02:16:09 +0800

    try

--- Lab4/lab4_template/Control.v
+++ Lab4/lab4_template/Control.v
@@ -17,10 +17,10 @@ module Control (
     assign {memRead,memtoReg,ALUOp,memWrite,ALUSrc,regWrite} = crtl;
     reg [9:0] b1 = 10'b01_0_00_00_0_0_0;
     reg [9:0] b2 = 10'b00_0_00_00_0_0_0;
+    assign flushCtrl = (PCSel == 0)?0:1; 
+    assign PCorR1 = (PCSel == 2'b01)?0:1;
     always@(*) begin
         PCSel = 2'b00;
-        flushCtrl = 0;
-        PCorR1 = 0;
         case(opcode)
             7'b0000011: crtl = 8'b1_01_00_0_1_1;  //load 
             7'b0010011: crtl = 8'b0_00_01_0_1_1;  // immediate add sub 
@@ -30,36 +30,28 @@ module Control (
             7'b1100111:begin
                 PCSel = 2'b10;
                 crtl = 8'b0_10_00_0_1_1; //jalr 
-                flushCtrl = 1;
-                PCorR1 = 1;
             end 
             7'b1101111:begin
                 PCSel = 2'b01;
                 crtl = 8'b0_10_00_0_1_1; //jal 
-                flushCtrl = 1;
             end
             7'b1100011:begin //  branch
                 crtl = 8'b0_00_00_0_0_0;
                 case(funct3)
                     3'b000: begin // beq
                         PCSel = (BrEq)?2'b01:2'b00;
-                        flushCtrl = (BrEq)?1:0;
                     end
                     3'b001: begin // bne
                         PCSel = (~BrEq)?2'b01:2'b00;
-                        flushCtrl = (~BrEq)?1:0;
                     end
                     3'b100: begin // blt
                         PCSel = (BrLT)?2'b01:2'b00;
-                        flushCtrl = (BrLT)?1:0;
                     end
                     3'b101: begin // bge
                         PCSel = (~BrLT)?2'b01:2'b00;
-                        flushCtrl = (~BrLT)?1:0;
                     end
                     default: begin
                         crtl = 8'b0_00_00_0_0_0;
-                        flushCtrl = 0;
                     end
                 endcase 
             end
